1988e3b8b0bc7c952201705c690d218bb8cec2bd
Your Name <you@example.com>
2020-03-07 15:21:32 -0700

    add, delete, and checkoff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const form = document.getElementById('add-task');
 const activeToDo = document.getElementById('active-todo');
-const toDoTableBody = document.getElementById('table-body');
+const toDoTableBody = document.getElementById('active-table-body');
+const completedTableBody = document.getElementById('completed-table-body');
 
+var i = 0;
 form.addEventListener('submit', function(event){
 
     event.preventDefault();
@@ -11,14 +13,36 @@ form.addEventListener('submit', function(event){
 
     var deleteButton = document.createElement('button');
     deleteButton.textContent = "Delete Task";
+
+    var completeCheckbox = document.createElement('input');
+    completeCheckbox.type = 'checkbox';
+    completeCheckbox.name = `task${i}`;
+
     deleteButton.addEventListener('click', function(event){
         var deleteRow = this.parentNode.parentNode;
         deleteRow.parentNode.removeChild(deleteRow);
     })
 
+    completeCheckbox.addEventListener('change', function(){
+        if (this.checked)
+        {
+            var deleteRow = document.getElementById(this.name);
+            var newRow = document.createElement('tr');
+            var newData = document.createElement('td');
+            
+            newData.textContent = deleteRow.childNodes[1].childNodes[0].textContent;
+            newRow.appendChild(newData);
+            completedTableBody.appendChild(newRow);
+
+            deleteRow.parentNode.removeChild(deleteRow);
+        }
+    })
+
     tdata.textContent = document.getElementById('task').value;
     tdata.appendChild(deleteButton);
-
+    
+    trow.id=`task${i++}`;
+    trow.appendChild(completeCheckbox);
     trow.appendChild(tdata);
     toDoTableBody.appendChild(trow);
 })
