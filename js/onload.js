20c69793299fdf46abb1cfdc89bacad8cf1a868e
unknown <andr3w.sc0tt86@gmail.com>
2020-03-09 14:10:27 -0600

    onload example

---
+++ js/onload.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+    var myHeading = document.querySelector('h1');
+    console.log(myHeading);
+}
--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,43 @@ myNavButton.addEventListener('click', function(event){
 
     myNav.classList.toggle('nav-open');
 })
+
+function Person (name, age, hobbies)
+{
+    this.name = name;
+    this.age = age;
+    this.hobbies = hobbies;
+    this.sayGoodbye = function()
+    {
+        document.body.innerHTML += `
+        <p>
+            This is <strong>${this.name}</strong>
+            saying <em>goodbye</em>!
+        </p>
+    `;
+    }    
+};
+
+var Bill = new Person("Bill", 55, ["Gettin' er done", "Muddin'", "Drinkin'"]);
+
+Person.prototype.introduction = function()
+{
+    var hobbiesString = `<ul>`;
+    this.hobbies.forEach(function (value, index)
+    {
+        hobbiesString += `<li>${value}</li>`
+    });
+    hobbiesString += `</ul>`;
+
+    document.body.innerHTML += `
+        <h2> ${this.name} </h2>
+        <dl>
+            <dt>Age</dt>
+            <dd> ${this.age} </dd>
+            <dt>Hobbies</dt>
+            <dd>${hobbiesString}</dd>
+        </dl>
+        `;
+}
+
+
