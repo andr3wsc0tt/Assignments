5e99646c5f2b6fdfa7cb5bde91b24b55bea67637
unknown <andr3w.sc0tt86@gmail.com>
2020-03-10 14:03:15 -0600

    test time

--- js/day6.js
+++ js/day6.js
@@ -1,57 +1,18 @@
-function Hangman(Guesses, Word){
-    
-    this.Guesses = Guesses;
-    this.Word = Word;
-    this.alive = true;
-
-    this.hidden = [...Array(Word.length).keys()];
-    this.shown = [];
-
-    this.makeGuess = function()
+class Hangman
+{
+    makeGuess = function()
     {
-        var finalword = "";
-        var letter = window.prompt("Choose a letter");
 
-        if (this.Word.includes(letter))
-        {
-            for (let i = 0; i < this.Word.length; i++)
-            {
-                if (this.Word[i] == letter && !this.shown.includes(i))
-                {
-                    this.hidden.splice(i, 1);
-                    this.shown.push(i);
-                }
-            }
-        }
-        else
-        {
-            this.Guesses -= 1;
-        }
-        
-        for (var show = 0; show < Word.length; show++)
-        {
-            if (this.shown.includes(show))
-            {
-                finalword += Word[show];
-
-            }
-            else
-            finalword += "*";
-        }
-        console.log(finalword);
-    }  
-
-    this.playGame = function()
-    {
-        while (this.Guesses > 0 && this.shown.length != this.Word.length)
-        {
-            console.log(this.shown.length);
-            console.log(this.Word.length);
-            this.makeGuess();
-        }
     }
 
-}
+    playGame = function ()
+    {
 
-Game = new Hangman(5, "HELLO");
-Game.playGame();
+    }
+}
+Game = new Hangman();
+
+var x = document.getElementById('formGet');
+x.addEventListener('submit', Game, function(event) {
+    event.preventDefault();
+});
