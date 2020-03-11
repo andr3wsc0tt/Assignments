4a87f64c6cab7b18c321665052d665f8e4835dd4
unknown <andr3w.sc0tt86@gmail.com>
2020-03-11 09:44:33 -0600

    hangman hanged!

--- js/day6.js
+++ js/day6.js
@@ -1,23 +1,62 @@
 var canvas = document.getElementById('hangCanvas');
 var ctx = canvas.getContext("2d");
 
-function drawCircle()
-{
+function drawCircle(x, y, r, s0, e0) {
     ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.arc(x, y, r, s0, e0);
     ctx.stroke();
 }
 
-function drawLine()
-{
+function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(300,150);
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
     ctx.stroke();
 }
 
-function Hangman(Guesses, Word){
-    
+function drawEyes(x1, y1)
+{
+    ctx.font = "9px Arial";
+    ctx.fillText("x", x1, y1);
+}
+function drawMan(guesses) {
+    switch (guesses) {
+        case 6:
+            drawCircle(150, 25, 10, 0, 2 * Math.PI); // head
+            break;
+        case 5:
+            drawLine(150, 35, 150, 55); // neck
+            break;
+        case 4:
+            drawLine(125, 55, 150, 55); // left arm
+            break;
+        case 3:
+            drawLine(175, 55, 150, 55); // right arm
+            break;
+        case 2:
+            drawLine(150, 55, 150, 80); // body
+            break;
+        case 1:
+            drawLine(150, 80, 135, 95); // left leg
+            break;
+        case 0:
+            drawLine(150, 80, 165, 95); // right leg
+            drawEyes(145, 25); // left eye
+            drawEyes(151, 25); // right eye
+            drawCircle(150, 32, 5, Math.PI+0.2, -0.2);
+            break;
+    }
+}
+function buildNoose()
+{
+    drawLine(5, 130, 55, 130);
+    drawLine(30, 130, 30, 10);
+    drawLine(30, 10, 150, 10);
+    drawLine(150, 10, 150, 15);
+}
+
+function Hangman(Guesses, Word) {
+
     this.Guesses = Guesses;
     this.Word = Word;
     this.alive = true;
@@ -25,37 +64,31 @@ function Hangman(Guesses, Word){
     this.hidden = [...Array(Word.length).keys()];
     this.shown = [];
 
-    this.makeGuess = function(letter)
-    {
+    buildNoose();
+
+    this.makeGuess = function (letter) {
         var finalword = "";
 
-        if (this.Word.includes(letter))
-        {
-            for (let i = 0; i < this.Word.length; i++)
-            {
-                if (this.Word[i] == letter && !this.shown.includes(i))
-                {
+        if (this.Word.includes(letter)) {
+            for (let i = 0; i < this.Word.length; i++) {
+                if (this.Word[i] == letter && !this.shown.includes(i)) {
                     this.hidden.splice(i, 1);
                     this.shown.push(i);
                 }
             }
         }
-        else
-        {
+        else {
             this.Guesses -= 1;
-            drawCircle();
-            drawLine();
+            drawMan(this.Guesses);
         }
-        
-        for (var show = 0; show < Word.length; show++)
-        {
-            if (this.shown.includes(show))
-            {
+
+        for (var show = 0; show < Word.length; show++) {
+            if (this.shown.includes(show)) {
                 finalword += Word[show];
 
             }
             else
-            finalword += "-";
+                finalword += "-";
         }
 
 
@@ -70,25 +103,27 @@ function Hangman(Guesses, Word){
         wordStat.append(word);
 
 
-    }  
+    }
 
-    this.playGame = function(letter)
-    {
+    this.playGame = function (letter) {
         this.makeGuess(letter);
-        if (this.Guesses == 0)
-            alert("GAME OVER");
         if (this.shown.length == this.Word.length)
             alert("YOU WIN!");
+        if (Game.Guesses == 0)
+            setTimeout(function(){
+                alert("GAME OVER");
+            },2)
     }
 
 }
 
-Game = new Hangman(5, "HELLO");
+Game = new Hangman(7, "HELLO");
 
 var x = document.getElementById('formGet');
-x.addEventListener('submit', function(event) {
+x.addEventListener('submit', function (event) {
     event.preventDefault();
 
     var letter = document.getElementById('letter').value;
     Game.playGame(letter);
+
 });
