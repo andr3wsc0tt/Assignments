c01355fd119a647d6ea30c0995e84a391be28535
Your Name <you@example.com>
2020-04-15 00:05:20 -0600

    added state

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,18 +3,30 @@ import * as React from 'react';
 export interface IButtonProps {
 }
 
-export default class Button extends React.Component<IButtonProps> {
+export interface IButtonState {
+  data: string
+}
 
-    componentDidMount(){
+export default class Button extends React.Component<IButtonProps, IButtonState> {
+
+    constructor(props : IButtonProps){
+      super(props);
+
+      this.state = {
+        data: "Click Me"
+      }
+    }
+    componentDidMount(){ //<Button/>
         onclick = () => {
-            const newChange = document.getElementById("change"); 
+            const newChange = document.getElementById("change");  //<button id="change">
             if (newChange !== null)
                 newChange.innerHTML = "Clicked!"
         }
     }
+
   public render() {
     return (
-      <button id="change">Click Me</button>
+      <button id="change">{this.state.data}</button>
     );
   }
 }
