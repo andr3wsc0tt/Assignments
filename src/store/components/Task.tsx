1ded1dac66d47eea2e18d088d3c5bd7dd96f0af4
Your Name <you@example.com>
2020-04-19 10:49:26 -0600

    all requirements fulfilled

--- src/store/components/Task.tsx
+++ src/store/components/Task.tsx
@@ -4,24 +4,25 @@ import {RootState} from '../../store'
 import {Task} from '../types/types'
 import {addTask, removeTask} from '../actions/actions'
 import {Card, Grid} from 'semantic-ui-react';
+import { workerData } from 'worker_threads';
 
 export interface ITaskAppProps {
     tasks: Task[],
     addTask: typeof addTask,
     taskNum: number
-    // addTask: typeof addTask,
     removeTask: typeof removeTask
 }
 
 export interface ITaskAppState {
     addValue : string,
-    removeValue : string
+    removeValue : string,
+    warning: string
 }
 export class TaskApp extends React.Component<ITaskAppProps, ITaskAppState> {
   
     constructor(props: ITaskAppProps){
         super(props);
-        this.state = {addValue: "", removeValue: ""};
+        this.state = {addValue: "", removeValue: "", warning: ""};
     }
 
     handleAdd = (e : React.FormEvent<HTMLFormElement> ) => {
@@ -29,23 +30,31 @@ export class TaskApp extends React.Component<ITaskAppProps, ITaskAppState> {
         let {addValue} = this.state;
         let {taskNum, addTask, removeTask} = this.props;
 
-
         addTask({id:taskNum, name:addValue});
+        this.setState({addValue : ""});
     }
     handleRemove = (e : React.FormEvent<HTMLFormElement> ) => {
         e.preventDefault();
         let {removeValue} = this.state;
-        let {taskNum, removeTask} = this.props;
+        let {removeTask, tasks} = this.props;
 
-        removeTask(Number(removeValue));
+        const result = tasks.filter(task => task.id == Number(removeValue));
+
+        if (result.length != 0){
+            removeTask(Number(removeValue));
+        }
+        else
+            this.setState({warning : "There is no such task"});
+
+            this.setState({removeValue : ""});
     }
 
     handleAddChange = (e : React.FormEvent<HTMLInputElement> ) => {
-        this.setState({addValue : e.currentTarget.value})
+        this.setState({addValue : e.currentTarget.value, warning: ""})
     }
 
     handleRemoveChange = (e : React.FormEvent<HTMLInputElement> ) => {
-        this.setState({removeValue : e.currentTarget.value})
+        this.setState({removeValue : e.currentTarget.value, warning: ""})
     }
 
     public render() {
@@ -59,7 +68,7 @@ export class TaskApp extends React.Component<ITaskAppProps, ITaskAppState> {
 
             <form onSubmit = {this.handleRemove}>
             <input type="text" value={this.state.removeValue} onChange={this.handleRemoveChange}/>
-            <input type="submit" value="Remove"/>
+            <input type="submit" value="Remove"/>{this.state.warning}
             </form>
           {tasks != null ? tasks.map(task => {
               return <Card>
